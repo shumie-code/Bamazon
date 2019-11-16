490cedd5eaa5dc1ad378efd16bc2f150d186f4a5
Petros Shumie <shumie.peter@gmail.com>
2019-11-16 01:27:55 -0500

    Show products function for displaying products

--- bamazon.js
+++ bamazon.js
@@ -18,6 +18,20 @@ var connection = mysql.createConnection({
 //connect to the mysql server and sql database
 connection.connect(function(err) {
     if (err) throw err;
-    // run the start function after the connection is made to prompt use
-    start();
+    // Show all products on BAMAZON
+    showProducts();
 });
+
+// Show all products
+function showProducts() {
+    var query = "Select * FROM products";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        for (var i=0; 1 < res.length; i++) {
+            console.log("Product ID: " + res[i].item_id + " || Product Name: " + res[i].product_name + " || Price: " + res[i].price);
+        }
+        // Finds product id and quantity for user
+        findProducts();
+    });
+};
+
