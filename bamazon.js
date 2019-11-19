a4eb2a4a56393570658dbee605b84b3e71b760b3
Petros Shumie <shumie.peter@gmail.com>
2019-11-18 19:08:08 -0500

    Created function for finding products and allowing the customer to search if product is available for purchase. If not then allows customer to purchase another item.

--- bamazon.js
+++ bamazon.js
@@ -35,3 +35,59 @@ function showProducts() {
     });
 };
 
+// Finds products quantity of items
+function findProducts() {
+    inquirer.prompt([{
+        name: "product ID",
+        type: "input",
+        message: "Enter product ID for item",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+                return true;
+            }
+            return false;
+        }
+       
+      },  {
+            name: "productUnits",
+            type: "input",
+            message: "How many units would you like?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false
+            }
+
+        }
+    }]).then(function(answer) {
+
+        //Checks the database for the selected product
+        var query = "Select stock_quantity, price, product_sales, department_name FROM products WHERE ?";
+        connection.query(query, { item_id: answer.productID}, function(err, res) {
+            if (err) throw err;
+
+            var available_stock = res[0].stock_quantity;
+            var price_per_unit = res[0].price;
+            var productSales = res[0],product_sales;
+            var prodcutDepartment = res[0].department_name;
+
+            // Checks if there is enough inventory to process purchase
+
+            if (available_stock >+ answer.productUnits) {
+
+                // Complete customer request for purchase
+                completePurchase(availble_stock, price_per_unit, productSales, prodcutDepartment, answer.productID, answer.productUnits);
+                } else {
+                    //Tells the user there isnt enough stock left
+                    console.log("There isnt enough stock left sorry.");
+
+                    //Lets Customer request another product
+                    requestProduct();
+                }
+        });
+    });
+};
+
+
+
