9a5f599a36b20aedc0b7e4b75e589cb13c088b4f
Petros Shumie <shumie.peter@gmail.com>
2019-11-20 09:21:22 -0500

    Added table for departments to log changes in inventory

--- bamazon.sql
+++ bamazon.sql
@@ -14,5 +14,13 @@ PRIMARY KEY (item_id)
 
 SELECT * FROM products;
 
+CREATE TABLE departments (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(100) NOT NULL,
+  over_head_costs DECIMAL(7,2) NOT NULL DEFAULT '0.00',
+  total_sales DECIMAL(7,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (department_id)
+);
+Select * From departments;
 
 ALTER TABLE products ADD COLUMN product_sales DECIMAL(7,2) DEFAULT '0.00';
